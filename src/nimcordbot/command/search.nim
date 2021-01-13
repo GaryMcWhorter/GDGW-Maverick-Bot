11848f8cf93ae93686b84ddfb4123c437a3620eb
Jason <beefers331@gmail.com>
2021-01-13 01:50:05 -0700

    Changed URL to duckduckgo not lmddgtfy

--- src/nimcordbot/command/search.nim
+++ src/nimcordbot/command/search.nim
@@ -1,7 +1,7 @@
 import command
 import std/uri
 proc search(dc: DiscordClient, s: string, dmsg: Message){.async.}= 
-  let res = "https://lmddgtfy.net/" & encodeurl(s, false)
+  let res = "https://duckduckgo.com/" & encodeurl(s, false)
   discard await dc.api.sendMessage(dmsg.channel_id, res)
 
 addCommand:
