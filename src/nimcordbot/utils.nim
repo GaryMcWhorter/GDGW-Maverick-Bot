05685555feb6fab251b39d7eb5bde0c266831942
GaryM-exkage <52535319+GaryM-exkage@users.noreply.github.com>
2021-02-13 01:25:03 -0800

    updated an example thanks command

--- src/nimcordbot/utils.nim
+++ src/nimcordbot/utils.nim
@@ -1,7 +1,7 @@
 import macros, os
 
 macro importCommands*(): untyped =
-  var bracket  = newNimNode(nnkBracket)
+  var bracket = newNimNode(nnkBracket)
   for x in walkDir("./src/nimcordbot/command", true):
     if(x.kind == pcFile):
       let split = x.path.splitFile()
@@ -14,19 +14,19 @@ macro importCommands*(): untyped =
         newNimNode(nnkPrefix).add(
           ident("/"),
           ident("command")
-        ),
-        bracket
-      )
+    ),
+    bracket
+  )
     )
   )
 
 proc startsWith*(msg: string, prefix: string): bool =
-    if msg.len == 0 or msg.len <= prefix.len:
-        return false
-    for i in 0 ..< prefix.len:
-        if prefix[i] != msg[i]:
-            return false
-    return true
+  if msg.len == 0 or msg.len <= prefix.len:
+    return false
+  for i in 0 ..< prefix.len:
+    if prefix[i] != msg[i]:
+      return false
+  return true
 
 proc getCommandInfo*(msg, prefix: string): (string, string) =
   var i = prefix.len
@@ -34,9 +34,12 @@ proc getCommandInfo*(msg, prefix: string): (string, string) =
     if msg[i] == ' ':
       break
     inc i
-  let params = 
+  let params =
     if i + 1 >= msg.len:
       ""
     else:
       msg[(i + 1)..^1]
   (msg[prefix.len..(i - 1)], params)
+
+proc toMention*(uid: string): string =
+  return "<@" & uid & ">"
